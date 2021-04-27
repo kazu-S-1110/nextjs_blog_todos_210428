4bef4ab3061331f1fc7d3796684bd83bbd01fd21
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-04-28 08:26:24 +0900

    環境構築が完了、Layoutコンポーネント作成

---
+++ components/Layout.js
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+
+export default function Layout({ children, title = 'Default title' }) {
+  return (
+    <div className="flex justify-center items-center flex-col min-h-screen text-white font-mono bg-gray-800">
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <main className="flex flex-1 justify-center items-center w-screen flex-col">
+        {children}
+      </main>
+      <footer className="w-full h-12 flex justify-center items-center border-t">
+        <a
+          className="flex items-center"
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+        </a>
+      </footer>
+    </div>
+  );
+}
