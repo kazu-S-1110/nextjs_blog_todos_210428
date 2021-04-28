442cad4f3ff73da1ab5e97b054b56af80be54495
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-04-28 12:34:34 +0900

    Authコンポーネント作成、index編集

--- components/Layout.js
+++ components/Layout.js
@@ -9,9 +9,9 @@ export default function Layout({ children, title = 'Default title' }) {
       <main className="flex flex-1 justify-center items-center w-screen flex-col">
         {children}
       </main>
-      <footer className="w-full h-12 flex justify-center items-center border-t">
+      <footer className="w-full h-12 flex justify-center items-center border-t bg-white">
         <a
-          className="flex items-center"
+          className="flex items-center text-black"
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
