d2394654100dd15c1f65fa6c60c94a90bcf730a1
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-05-04 19:54:18 +0900

    taskの詳細ページを作成

--- pages/tasks/[id].js
+++ pages/tasks/[id].js
@@ -25,7 +25,35 @@ export default function Post({ staticTask, id }) {
     return <div>Loading...</div>;
   }
 
-  return <Layout title={task.title}></Layout>;
+  return (
+    <Layout title={task.title}>
+      <span className="mb-4">
+        {'ID : '}
+        {task.id}
+      </span>
+      <p className="mb-4 text-xl font-bold">{task.title}</p>
+      <p className="mb-12">{task.created_at}</p>
+      <Link href="/task-page">
+        <div className="flex cursor-pointer mt-8">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 mr-3"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
+            />
+          </svg>
+          <span>Back to task-page</span>
+        </div>
+      </Link>
+    </Layout>
+  );
 }
 
 export async function getStaticPaths() {
